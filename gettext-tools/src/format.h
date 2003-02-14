235a88799b3a6ceb22a5b26246ef38341014d9f0
Bruno Haible <bruno@clisp.org>
2003-02-14 14:08:22 +0000

    Moved here from src/format.h.

---
+++ gettext-tools/src/format.h
@@ -0,0 +1,84 @@
+/* Format strings.
+   Copyright (C) 2001-2002 Free Software Foundation, Inc.
+   Written by Bruno Haible <haible@clisp.cons.org>, 2001.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+
+#ifndef _FORMAT_H
+#define _FORMAT_H
+
+#include <stdbool.h>
+
+#include "pos.h"	/* Get lex_pos_ty.  */
+#include "message.h"	/* Get NFORMATS.  */
+
+/* This structure describes a format string parser for a language.  */
+struct formatstring_parser
+{
+  /* Parse the given string as a format string.
+     Return a freshly allocated structure describing
+       1. the argument types/names needed for the format string,
+       2. the total number of format directives.
+     Return NULL if the string is not a valid format string.  */
+  void * (*parse) (const char *string);
+
+  /* Free a format string descriptor, returned by parse().  */
+  void (*free) (void *descr);
+
+  /* Return the number of format directives.
+     A string that can be output literally has 0 format directives.  */
+  int (*get_number_of_directives) (void *descr);
+
+  /* Verify that the argument types/names in msgid_descr and those in
+     msgstr_descr are the same (if equality=true), or (if equality=false)
+     that those of msgid_descr extend those of msgstr_descr (i.e.
+     msgstr_descr may omit some of the arguments of msgid_descr).
+     If not, signal an error using
+       error_with_progname = false;
+       error_at_line (0, 0, pos->file_name, pos->line_number, ...);
+       error_with_progname = true;
+     (but only if noisy=true) and return true.  Otherwise return false.  */
+  bool (*check) (const lex_pos_ty *pos, void *msgid_descr, void *msgstr_descr, bool equality, bool noisy, const char *pretty_msgstr);
+};
+
+/* Format string parsers, each defined in its own file.  */
+extern struct formatstring_parser formatstring_c;
+extern struct formatstring_parser formatstring_python;
+extern struct formatstring_parser formatstring_lisp;
+extern struct formatstring_parser formatstring_elisp;
+extern struct formatstring_parser formatstring_librep;
+extern struct formatstring_parser formatstring_smalltalk;
+extern struct formatstring_parser formatstring_java;
+extern struct formatstring_parser formatstring_awk;
+extern struct formatstring_parser formatstring_pascal;
+extern struct formatstring_parser formatstring_ycp;
+extern struct formatstring_parser formatstring_tcl;
+extern struct formatstring_parser formatstring_php;
+
+/* Table of all format string parsers.  */
+extern struct formatstring_parser *formatstring_parsers[NFORMATS];
+
+/* Returns an array of the ISO C 99 <inttypes.h> format directives
+   contained in the argument string.  *intervalsp is assigned to a freshly
+   allocated array of intervals (startpos pointing to '<', endpos to '>'),
+   and *lengthp is assigned to the number of intervals in this array.  */
+struct interval
+{
+  size_t startpos;
+  size_t endpos;
+};
+extern void get_c99_format_directives (const char *string, struct interval **intervalsp, size_t *lengthp);
+
+#endif /* _FORMAT_H */
